d69d4fbf7309b3d017d5c6c52018317fd0f0ac23
emanueljorge <emanuel.jorge.130@gmail.com>
2022-01-04 20:30:54 +0100

    final

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -12,7 +12,8 @@ class RecipeDetail extends StatefulWidget {
 
 //  Add _RecipeDetailState here
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  // Add _sliderVal here
+  int _sliderVal = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +54,33 @@ class _RecipeDetailState extends State<RecipeDetail> {
                 itemBuilder: (BuildContext context, int index) {
                   final ingredient = widget.recipe.ingredients[index];
                   // 9
-                  // TODO: Add ingredient.quantity
-                  return Text(
-                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                  // Add ingredient.quantity
+                  return Text('${ingredient.quantity * _sliderVal} '
+                      '${ingredient.measure} '
+                      '${ingredient.name}');
                 },
               ),
             ),
-            // TODO: Add Slider() here
+            // Add Slider() here
+            Slider(
+              // 10
+              min: 1,
+              max: 10,
+              divisions: 10,
+              // 11
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              // 12
+              value: _sliderVal.toDouble(),
+              // 13
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              // 14
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
